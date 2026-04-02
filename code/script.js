4526d97abeed22276a48979e0049bf396e70d17b
Uma Thiyagarajan <uma.thiyaga@outlook.com>
2026-04-02 16:03:54 +0200

    Complete step2: Food choice

--- code/script.js
+++ code/script.js
@@ -3,11 +3,33 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Explore? - Click 'OK' to begin.`
 )
 
-const name = prompt(`What is your name?`);
-alert(`Hi ${name}, Welcome to our pizzeria!`);
+const customerName = prompt(`What is your name?`);
+alert(`Hi ${customerName}, Welcome to our pizzeria!`);
 
 // Step 2 - Food choice
-// Your code goes here
+const foodChoice = prompt(
+  "What would you like to order?\n" +
+  "\n" +
+  "   We have the following menu:\n" +
+  "     1. Pizza\n" +
+  "     2. Pasta\n" +
+  "     3. Salad\n" +
+  "\n" +
+  "Please enter the number of your choice."
+);
+
+if (foodChoice === "1") {
+  menu = "Pizza";
+  alert(`${menu} mode activated. Prepare for deliciousness!`);
+} else if (foodChoice === "2") {
+  menu = "Pasta";
+  alert(`You chose ${menu} — that’s amore!`);
+} else if (foodChoice === "3") {
+  menu = "Salad";
+  alert(`${menu} selected. Your body says thank you!`);
+}
+
+
 
 // Step 3 - Subtype choice
 // Your code goes here
